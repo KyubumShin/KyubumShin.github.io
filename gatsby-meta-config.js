a37eb53ab4b3a682975cf1c54cfba640ce9bb3c4
Kyubum Shin <kbum0617@naver.com>
2021-05-02 23:49:24 +0900

    update post

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -21,6 +21,9 @@ module.exports = {
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
